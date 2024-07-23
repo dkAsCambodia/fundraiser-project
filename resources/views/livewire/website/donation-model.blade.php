ce9489c8585bd6331c3177530a84d1c71b0b93d7
SushilLoma <141897330+SushilLoma@users.noreply.github.com>
2024-07-23 15:35:25 +0700

    hover problem and size issues solve

--- resources/views/livewire/website/donation-model.blade.php
+++ resources/views/livewire/website/donation-model.blade.php
@@ -314,6 +314,10 @@
             .donationBox .holder {
                 border-radius: 0;
             }
+            .donationBox .donation_right {
+                width: 100%;
+                height: 500px !important;
+            }
         }
 
 </style>
@@ -1006,7 +1010,7 @@
                                 USD United State Doller</p>
                         </div>
                         <div class="bottom_price">
-                            <form action="{{ route('paypal.checkout') }}" method="post"> 
+                            <form action="{{ route('paypal.checkout') }}" method="post">
                                 @csrf
                                 <input type="hidden" class="Final_amount" name="amount"
                                     value="{{ currency($causeDetails->default_amount) }}{{Session::get('sessLocation')?->curency_code=='KHR' ? '00' : ''}}" />
